60280665338bb9ccf436ce75d9ed7e960c742b9d
linuxenko <linuxenko@yahoo.com>
2016-04-21 19:40:54 +0300

    New todo style

--- app/component/NewTodo.js
+++ app/component/NewTodo.js
@@ -2,10 +2,32 @@ import React, {Component} from 'react';
 
 
 export default class NewTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mode : 'button'
+    }
+  }
+
+  toggleMode() {
+    this.setState({
+      mode : this.state.mode === 'layout' ? '' : 'layout'
+    });
+  }
+
   render() {
     return (
-      <div className="new-todo">
-        <span className="new-todo--icon">+</span>
+      <div className={this.state.mode === 'button' ? 'new-todo' : 'new-todo layout-new-todo'}
+        onClick={this.toggleMode.bind(this)}>
+        {this.state.mode === 'button' ? <span className="new-todo--icon">+</span> :
+
+        <div className="layout-new-todo--wrapper">
+          <h3>New todo</h3>
+          <input type="text" placeholder="Todo ..." />
+
+          <span className="button">Create Todo</span>
+        </div>
+       }
       </div>
     )
   }
